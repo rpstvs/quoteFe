90f8ea6de38be297dcd21204c2dec9fdae6c1a38
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-11-11 16:31:17 +0000

    fixes

--- quoteFe/vite.config.js
+++ quoteFe/vite.config.js
@@ -5,14 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/quoteFe/",
-  publicPath: process.env.NODE_ENV === 'production' ? '/quoteFe/' : '/',
-  plugins: [
-    vue(),
-  ],
+  publicPath: '/',
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
